649da21f1eb79550a9e6c723cc359ae920362a71
Kushagra23d <kushagra.23.cse@gmail.com>
2021-11-14 14:56:02 +0530

    Add project files.

---
+++ ThreadPoolingExample/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Threading;
+
+
+namespace ThreadPoolingExample
+{
+    class Program
+    {           
+
+        static void Main(string[] args)
+        {
+
+            string msg = "Hello";
+            WaitCallback wc = new WaitCallback(Printf);
+            ThreadPool.QueueUserWorkItem(wc, msg);
+            Console.ReadLine();
+
+        
+        }
+
+        public static void Printf(object ms) 
+        {
+            while(true){
+                Console.WriteLine($"{DateTime.Now} {ms}");
+                Thread.Sleep(1000);
+            }
+        }
+
+    }
+}
